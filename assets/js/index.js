b559b2e3568145f352d8fd7d541c330d40abb004
Keikishim2 <Keikishim2@gmail.com>
2021-06-25 12:09:56 +0800

    Output

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+
+
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountains_front = document.getElementById('mountains_front');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains_behind.style.top = value * 0.05 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 6 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+})
